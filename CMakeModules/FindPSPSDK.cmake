3ce8c247c786ca7eb146f46a93b4acfeab0d7bb7
Tobias Loose <TobiasLoose@gmx.de>
2013-11-22 01:55:18 +0100

    Add psp build option.
    There is now an option to build for the psp with the pspsdk, for travic-ci
    with qt-console and for qt-widget.

--- CMakeModules/FindPSPSDK.cmake
+++ CMakeModules/FindPSPSDK.cmake
@@ -4,23 +4,29 @@
 #  PSPSDK_INCLUDE_DIR - The PSPSDK include directories
 #  PSPSDK_LIB - The libraries requested with the components field
 #  PSPSDK_REQUIRED_LIB - The libriries the PSPSDK needs always
-#  PSPSDK_CFLAGS  - The CFLAGS to use 
-# 
-# does not but will in future versions:
-#  PSPSDK_CXX_COMPILER - The Compiler to use with PSPSDK
-#  PSPSDK_CXX_LINKER   - The Linker to use with PSPSDK
+#  PSPSDK_CFLAGS  - The CFLAGS to use
+#  PSPSDK_PATH - The output of psp-config --pspsdk-path
+#  PSPSDK_PREFIX - The output of psp-config --psp-prefix
+#  PSPSDK_CXX_COMPILER - The PSPSDK CXX Compilers path
+#  PSPSDK_CXX_LINKER - The PSPSDK CXX Linker command
 
-
-#get pspsdk paths
+#find the psp-config progams absolute path
+#psp-config needs to be reachable via the system shell (PATH)
 find_program(PSP_CONFIG_PROGRAM psp-config)
 
-if(PSP_CONFIG_PROGRAM) 
-    execute_process(COMMAND psp-config --pspsdk-path OUTPUT_VARIABLE PSPSDK OUTPUT_STRIP_TRAILING_WHITESPACE)
-    execute_process(COMMAND psp-config --psp-prefix  OUTPUT_VARIABLE PSPDIR OUTPUT_STRIP_TRAILING_WHITESPACE)
+#TODO: check if something is REQUIRED and throw errors instead of messages
+if(PSP_CONFIG_PROGRAM)
+    find_program(PSPSDK_CXX_COMPILER psp-g++)
+    find_program(PSPSDK_CXX_LINKER psp-gcc)
     
-    set(PSPSDK_INCLUDE_DIR "${PSPSDK}/include")
+    #ask psp-config for the 
+    execute_process(COMMAND psp-config --pspsdk-path OUTPUT_VARIABLE PSPSDK_PATH OUTPUT_STRIP_TRAILING_WHITESPACE)
+    execute_process(COMMAND psp-config --psp-prefix  OUTPUT_VARIABLE PSPSDK_PREFIX OUTPUT_STRIP_TRAILING_WHITESPACE)
     
-    set(PSPSDK_LIB_PATH "${PSPSDK}/lib" "${PSPSDK}/include/libc")
+    set(PSPSDK_INCLUDE_DIR "${PSPSDK_PATH}/include")
+    
+    #find libs which the user specified via the COMPONENTS parameter
+    set(PSPSDK_LIB_PATH "${PSPSDK_PATH}/lib" "${PSPSDK_PATH}/include/libc")
     foreach(_COMPONENT ${PSPSDK_FIND_COMPONENTS})
         find_library(PSPSDK_${_COMPONENT} NAMES ${_COMPONENT} HINTS ${PSPSDK_LIB_PATH})
         if(NOT PSPSDK_${_COMPONENT})
@@ -30,7 +36,7 @@ if(PSP_CONFIG_PROGRAM)
         endif()
     endforeach()
     
-    #link with the required pspsdk libs
+    #find libs which pspsdk does require to link even if the programs does not need one of them directly
     foreach(_COMPONENT pspdebug pspdisplay pspge pspctrl pspsdk c pspnet pspnet_inet pspnet_apctl pspnet_resolver psputility pspuser)
         find_library(PSPSDK_${_COMPONENT} NAMES ${_COMPONENT} HINTS ${PSPSDK_LIB_PATH})
         if(NOT PSPSDK_${_COMPONENT})
@@ -39,13 +45,12 @@ if(PSP_CONFIG_PROGRAM)
             set(PSPSDK_REQUIRED_LIB ${PSPSDK_REQUIRED_LIB} ${PSPSDK_${_COMPONENT}})
         endif()
     endforeach()
-    
 endif()
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LIBXML2_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(PSPSDK  DEFAULT_MSG
-                                  PSPSDK_LIB PSPSDK_REQUIRED_LIB PSPSDK_INCLUDE_DIR)
+                                  PSPSDK_CXX_LINKER PSPSDK_CXX_COMPILER PSPSDK_PATH PSPSDK_PREFIX PSPSDK_LIB PSPSDK_REQUIRED_LIB PSPSDK_INCLUDE_DIR)
 
 mark_as_advanced(PSPSDK_INCLUDE_DIR PSPSDK_LIB)
