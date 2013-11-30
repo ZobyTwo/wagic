704245744decb98d0875cf0439f1b6d516bd57ff
Tobias Loose <TobiasLoose@gmx.de>
2013-11-30 11:59:18 +0100

    Add psp build and qt widget build (commented out)

--- travis-script.sh
+++ travis-script.sh
@@ -1,22 +1,11 @@
 #!/bin/sh -ex
 
-# let's also try the sdl binary
+#this does not work currenly as travic has no sdl2 installable
 #mkdir build_sdl
 #cd build_sdl
 #cmake -Dbackend_sdl=ON -Dbackend_qt_console=OFF ..
 #make -j8
 #cd ..
-
-# we're building a Qt version with GUI here
-mkdir qt-gui-build
-cd qt-gui-build
-qmake ../projects/mtg/wagic-qt.pro CONFIG+=release CONFIG+=graphics
-make -j 8
-cd ..
-
-# let's try an Intel linux binary in debug text-mode-only
-qmake projects/mtg/wagic-qt.pro CONFIG+=console CONFIG+=debug DEFINES+=CAPTURE_STDERR
-make -j 8
 
 # we create resource package
 #cd projects/mtg/bin/Res
@@ -26,12 +15,26 @@ make -j 8
 #mv core_*.zip ../../../../core.zip
 #cd ../../../..
 
+# psp build
+mkdir build_psp
+cd build_psp
+cmake -DCMAKE_TOOLCHAIN_FILE=../CMakeModules/psp.toolchain.cmake ..
+make -j8
+cd ..
+
 # let's try an qt-console build
 mkdir build_qt_console
 cd build_qt_console
 cmake -Dbackend_qt_console=ON ..
 make -j8
 cd ..
+
+#and an qt-widget build
+#mkdir build_qt_widget
+#cd build_qt_widget
+#cmake -Dbackend_qt_widget=ON -Dbackend_qt_console=OFF ..
+#make -j8
+#cd ..
 
 # and finish by running the testsuite
 cd projects/mtg
